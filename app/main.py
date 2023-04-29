982c35fa0d06e869f25fbae58c76e060094e0fcd
Nijad Huseynov <nijad.huseyn@gmail.com>
2023-04-29 13:47:01 +0400

    added visualization and fixed bugs

--- app/main.py
+++ app/main.py
@@ -1,9 +1,11 @@
 import atexit
 
+import numpy as np
+
 from config import get_general_config
 from game_server_client import GameServerClient
 from grid_world import QLearning
-from util import save_q_table_state
+from util import save_q_table_state, ROW, COL, map_action_to_direction
 
 if __name__ == '__main__':
     user_id = get_general_config('user_id')
@@ -11,10 +13,12 @@ if __name__ == '__main__':
     team_id = get_general_config('team_id')
     world_id = get_general_config('world_id')
     gsc = GameServerClient(user_id, api_key, team_id, is_mocked=True)
+    np.random.seed(4)
 
-    print(gsc.make_move(0, "N"))
-    print(gsc.make_move(0, "N"))
-
-    #qlearning = QLearning(gsc, world_id)
-    #atexit.register(save_q_table_state, world_id, qlearning.q_table)
-    #qlearning.start_learning()
+    qlearning = QLearning(gsc, world_id, alpha=0.2, gamma=0.9, epsilon=0.3)
+    atexit.register(save_q_table_state, world_id, qlearning.q_table)
+    for i in range(100000):
+        qlearning.start_learning()
+    for i in range(ROW):
+        for j in range(COL):
+            print(str(i) + "," + str(j), " : ",  map_action_to_direction(np.argmax(qlearning.q_table[i, j])), np.max(qlearning.q_table[i, j]))
--- app/util.py
+++ app/util.py
@@ -1,7 +1,8 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
-COL = 40
-ROW = 40
+COL = 10
+ROW = 10
 
 directions = {"N": (0, 1), "E": (1, 0), "S": (0, -1), "W": (-1, 0)}  # (x, y) pair
 
@@ -18,7 +19,7 @@ def load_q_table_state(world_id):
 
 
 def is_valid_position(x, y) -> bool:
-    return 0 <= x < COL and 0 <= y < ROW
+    return 0 <= x < ROW and 0 <= y < COL
 
 
 def map_action_to_direction(action):
@@ -29,6 +30,26 @@ def map_action_to_direction(action):
     if action == 2:
         return "S"
     return "W"
+
+
+def map_action_to_arrows(action):
+    if action == 0:
+        return "↑"
+    if action == 1:
+        return "→"
+    if action == 2:
+        return "↓"
+    return "←"
+
+
+def map_action_to_arrows_local(action):
+    if action == 0:
+        return "→"
+    if action == 1:
+        return "↑"
+    if action == 2:
+        return "←"
+    return "↓"
 
 
 def map_direction_to_action(direction):
@@ -43,3 +64,26 @@ def map_direction_to_action(direction):
 
 def get_valid_moves_from(x, y):
     return [k for k, v in directions.items() if is_valid_position(x + v[0], y + v[1])]
+
+
+def visualize_the_world():
+    q_tab = load_q_table_state(34)
+
+    grid = np.argmax(q_tab, axis=2)
+    row = grid.shape[1]
+    col = grid.shape[0]
+    zeros = np.zeros((col, row))
+
+    plt.figure(figsize=(6, 6), dpi=100)
+
+    for i in range(grid.shape[0]):
+        for j in range(grid.shape[1]):
+            plt.text(j, i, map_action_to_arrows_local(grid[i, j]),
+                     ha='center', va='center', color='red')
+
+    plt.imshow(zeros, cmap='Greys', interpolation='nearest')
+    plt.gca().invert_yaxis()
+    plt.show()
+
+
+visualize_the_world()
